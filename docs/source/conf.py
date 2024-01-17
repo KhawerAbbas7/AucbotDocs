f3e5d279feab6695f118b1e323ec2bc8c17ed176
KhawerAbbas7 <85282172+KhawerAbbas7@users.noreply.github.com>
2024-01-17 12:16:55 +0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Aucbot'
+copyright = '2021, Khawer'
+author = 'Khawer'
 
 release = '0.1'
-version = '0.1.0'
+version = '2.6'
 
 # -- General configuration
 
